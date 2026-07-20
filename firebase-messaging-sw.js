c59cb98e0fde8d7c88b018a1fde2c4ebc055b9f6
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-07-20 19:14:23 +0530

    Update Firebase configuration in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBuVQISkaKjflN7qQNy35hW7VVWIb-AL2Y",
+  authDomain: "odia-ratna-kalakar-sangha.firebaseapp.com",
+  projectId: "odia-ratna-kalakar-sangha",
+  storageBucket: "odia-ratna-kalakar-sangha.firebasestorage.app",
+  messagingSenderId: "462765943345",
+  appId: "1:462765943345:web:167b7dd0eaba5786830310"
 });
 
 const messaging = firebase.messaging();
