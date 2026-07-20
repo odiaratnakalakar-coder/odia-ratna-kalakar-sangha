cd10cbbb65e973e37df1c89550bba8437a54e1a5
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-07-20 23:17:46 +0530

    Update Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getFirestore } from "http
-
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyBuVQISkaKjflN7qQNy35hW7VVWIb-AL2Y",
   authDomain: "odia-ratna-kalakar-sangha.firebaseapp.com",
   projectId: "odia-ratna-kalakar-sangha",
@@ -16,4 +17,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const messaging = getMessaging(app);
+
 export { db, storage, messaging };
