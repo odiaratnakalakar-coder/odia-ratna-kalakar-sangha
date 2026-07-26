afb4102fda73d97c4cdf92b294e55cfbe0405024
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-07-26 17:33:18 +0530

    Add Firebase Authentication import to firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyBuVQISkaKjflN7qQNy35hW7VVWIb-AL2Y",
   authDomain: "odia-ratna-kalakar-sangha.firebaseapp.com",
