fcf32e75feea550185b9a6e728ab23aa00314122
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-07-26 17:34:25 +0530

    Add Firebase Auth initialization

--- firebase.js
+++ firebase.js
@@ -17,5 +17,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const messaging = getMessaging(app);
-
+const auth = getAuth(app);
 export { db, storage, messaging };
