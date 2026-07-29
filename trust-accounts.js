ea456dea180cb9108dff89ff00d5d6b53c8fa885
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-07-29 23:57:57 +0530

    Refactor loadDashboard function for error handling

--- trust-accounts.js
+++ trust-accounts.js
@@ -5,15 +5,18 @@ import {
   getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-console.log("Trust Accounts Loaded");
-async function loadDashboard() {
-
-    // Total Members
+console.log("Trust Accounts Loaded");async function loadDashboard() {
+  try {
     const memberSnap = await getDocs(collection(db, "members"));
 
-    document.getElementById("members").textContent =
-        memberSnap.size;
+    document.getElementById("members").textContent = memberSnap.size;
 
+    console.log("Members:", memberSnap.size);
+
+  } catch (error) {
+    console.error("Firestore Error:", error);
+    alert(error.message);
+  }
 }
 
 loadDashboard();
