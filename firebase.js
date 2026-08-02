23fe263f011f60d3b9542224c12e04208fa63c24
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-08-02 09:06:39 +0530

    Remove messaging export from firebase.js

--- firebase.js
+++ firebase.js
@@ -16,6 +16,6 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const storage = getStorage(app);
-const messaging = getMessaging(app);
+
 const auth = getAuth(app);
-export { db, storage, messaging, auth };
+export { db, storage, auth };
